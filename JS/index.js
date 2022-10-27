3c317c1fcea51b6a727c933333d2681e4581207c
Kevin Reis Barbosa <kevinreis1@gmail.com>
2022-10-27 17:31:25 -0300

    Direito

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typing", {
+    strings:["","Desenvolvedor Web", "Estudante de Direito", "Ex-Jogador de futebol de base","Júnior Back-End"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+
+})
